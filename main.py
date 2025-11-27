49b7f4c11acc405b071ab61e30f01b90baf355e4
sharipovr <sharipov.rustem@gmail.com>
2025-11-27 11:56:05 -0600

    Made a first connection/client with Gemini model

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from aiagent-python!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 
-if __name__ == "__main__":
-    main()
+model = "gemini-2.0-flash-001"
+content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+res = client.models.generate_content(model=model, contents=content)   
+
+print(res.text)
+
+print("Prompt tokens:", res.usage_metadata.prompt_token_count)
+print("Response tokens:", res.usage_metadata.candidates_token_count)
+
